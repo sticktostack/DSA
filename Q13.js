b22ab813941799bdeac045e239097a3806d83210
sticktostack <rajdeep6153@gmail.com>
2025-12-26 01:50:46 +0530

    third commit

--- Q13.js
+++ Q13.js
@@ -1,3 +1,5 @@
+// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
 var reverseWords = function(s) {
   rev = s.split(" ").reverse()
   let result = ""
