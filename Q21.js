d6ae8a239c53e3404a384856f61f6ebdeca36809
sticktostack <rajdeep6153@gmail.com>
2026-01-03 01:20:27 +0530

    Q21

---
+++ Q21.js
@@ -0,0 +1,20 @@
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+
+var missingNumber = function(nums) {
+    let missingnum;
+for(let i=0; i<nums.length; i++){
+    let found = false
+    for(let j=0; j<nums.length; j++){
+        if(nums[j] === i){
+            found = true
+            break
+        }
+    }
+    if(!found){
+        missingnum = i
+        break
+    }
+}
+ return missingnum
+};
