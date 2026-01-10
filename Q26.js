014818093a277db80498e8ac687bc64f26a3eab3
sticktostack <rajdeep6153@gmail.com>
2026-01-10 17:48:20 +0530

    Q26

---
+++ Q26.js
@@ -0,0 +1,7 @@
+// Given a string s, return the number of segments in the string.
+
+// A segment is defined to be a contiguous sequence of non-space characters.
+
+s = "hello my name is rajdeep"
+let seg = s.split(" ")
+console.log(seg.length)
